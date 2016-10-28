9f7dbd37418ecc6b58f70ce516b06cb3894ac937
MooseMadness <ggg12345jjj@gmail.com>
2016-10-28 13:44:01 +0300

    Create base class for moving object (CellObjectScript.cs)

--- Project/Sorter of warehouse/Assets/Scripts/CellObjectScript.cs
+++ Project/Sorter of warehouse/Assets/Scripts/CellObjectScript.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CellObjectScript : MonoBehaviour {
+//Базовый класс для объектов которые могут перемещаться по клеткам
+public class CellObjectScript : MonoBehaviour
+{
+    //ссылка на клетку в которой находится объект
+    public CellScript currCell;
+    //смещение координат объекта относительно координат клетки
+    public Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //истина если объект движется
+    protected bool moving { get; private set; }
+
+    //корутина перемещающая объект в другую клетку
+    public IEnumerator MoveToCell(CellScript cell, float moveSpeed)
+    {
+        moving = true;
+        //по игровой механике объект "переходит" в клетку до того как завершится визуальный переход
+        currCell = cell;
+        while(cell.transform.position != transform.position - offset)
+        {
+            float deltaSpeed = moveSpeed * Time.deltaTime;
+            Vector3 movement = cell.transform.position - (transform.position - offset);
+            if (movement.sqrMagnitude > deltaSpeed * deltaSpeed)
+            {
+                movement = movement.normalized * deltaSpeed;
+                transform.Translate(movement); 
+            }
+            else
+            {
+                transform.position = cell.transform.position + offset;
+            }
+            yield return null; //продолжить в следующем update
+        }
+        EndMoveAction();
+        moving = false;
+    }
+
+    //чтобы выполнить доп. действия полсе окончания движения
+    //потомки должны перегрузить этот метод
+    protected virtual void EndMoveAction() { }
+
+    private void Start()
+    {
+        moving = false;
+        if (currCell != null)
+            transform.position = currCell.transform.position + offset;
+    }
 }
